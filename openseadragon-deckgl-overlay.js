d74179109115c2a3f653f3b55d9d46e1dff92e33
ynitto <ynitto7@gmail.com>
2022-08-15 16:29:02 +0900

    remove require.

--- openseadragon-deckgl-overlay.js
+++ openseadragon-deckgl-overlay.js
@@ -90,14 +90,14 @@
     }
   };
 
-  (function (factory) {
+  (function(factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
+        var v = factory(exports);
         if (v !== undefined) module.exports = v;
     } else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
+        define(["exports"], factory);
     }
-  })(function (require, exports) {
+  })(function(exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.DeckGLOverlay = Overlay;
